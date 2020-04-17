5ad6bf6c9f6281e4c31aea5847f3c4d65ac4561b
MADZEROPIE <ask_ii1@mail.ru>
2020-04-17 20:40:17 +0300

    advanced(not) sort test

--- samples/sample_tqueue.cpp
+++ samples/sample_tqueue.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-#include "tstack.h"
-#include "tqueue.h"
-#include "List.h"
 #include "heap.h"
 #include <algorithm>
 
-const int N = 15;
+const int N = rand()%150;
 
 
 int main(int argc, char** argv)
@@ -13,13 +10,14 @@ int main(int argc, char** argv)
 	setlocale(LC_ALL, "Russian");
 	std::vector<Prior_Elem<int> > a, b;
 	for (int i = 0; i < N; ++i) {
-		int r = i;// std::rand();
+		int r = std::rand();
 		a.push_back(Prior_Elem<int>(r, r));
-		//b.push_back(Prior_Elem<int>(r, r));
+		b.push_back(Prior_Elem<int>(r, r));
 	}
 	TPriority_queue<int>::heapSort(a);
+	std::sort(b.begin(), b.end(), [](const Prior_Elem<int>& c, const Prior_Elem<int>& d) {return c.prior < d.prior; });
 	for (int i = 0; i < N; ++i) {
-		std::cout << a[i].elem << '\n';
+		std::cout << a[i].elem << "\t\t\t"<< b[i].elem<<'\n';
 	}
 	return 0;
 }
